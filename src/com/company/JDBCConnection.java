1ba3417424e41d8feec73ccbbbd42b754e8ab7c3
valente alvarez <valvarez@basecampcodingacademy.org>
2018-03-05 13:12:43 -0600

    created a class that will connect to the datatbase

---
+++ src/com/company/JDBCConnection.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.sql.*;
+
+public class JDBCConnection {
+    public static Connection getDatabase() {
+        try{
+            return DriverManager.getConnection("jdbc:postgresql:Animals", "basecamp", "localpassword");
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
